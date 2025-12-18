a3a9ff64c3fa81d3aadea393adec981718d24ae4
Elox <hery.elouen@gmail.com>
2025-12-18 17:54:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function toggleMenu() {
   const menu = document.querySelector('.mobile-menu');
-  menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
+  const spans = document.querySelectorAll('.burger span');
+  
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+    spans[0].style.transform = "none";
+    spans[1].style.opacity = "1";
+    spans[2].style.transform = "none";
+  } else {
+    menu.style.display = "flex";
+    // Animation petite croix pour le burger
+    spans[0].style.transform = "rotate(45deg) translate(5px, 5px)";
+    spans[1].style.opacity = "0";
+    spans[2].style.transform = "rotate(-45deg) translate(7px, -6px)";
+  }
 }
